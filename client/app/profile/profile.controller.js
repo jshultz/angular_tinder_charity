21b2b5cd92e414d9482ee3497918f78696c91882
Jason Shultz <jasshultz@gmail.com>
2015-07-07 07:36:01 -0600

    working on subnav

--- client/app/profile/profile.controller.js
+++ client/app/profile/profile.controller.js
@@ -4,6 +4,14 @@ angular.module('charityApp')
   .controller('ProfileCtrl', function ($scope, usersFactory, user) {
     $scope.message = 'Hello';
 
+    $scope.menu = [{
+      'title': 'Home',
+      'link': '/'
+    }, {
+      'title': 'About',
+      'link': '/about'
+    }];
+
 	usersFactory.getProfile(user).then(function(response){
 		if (response != null) {
 		  $scope.user = response;
