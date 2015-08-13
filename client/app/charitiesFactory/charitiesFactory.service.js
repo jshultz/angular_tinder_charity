b6c361223f19586f104b8f6308426b6312a4bebb
Jason Shultz <jasshultz@gmail.com>
2015-08-13 08:51:16 -0600

    working on normalization

--- client/app/charitiesFactory/charitiesFactory.service.js
+++ client/app/charitiesFactory/charitiesFactory.service.js
@@ -1,19 +1,42 @@
 'use strict';
 
+function extend(base) {
+    var parts = Array.prototype.slice.call(arguments, 1);
+    parts.forEach(function (p) {
+        if (p && typeof (p) === 'object') {
+            for (var k in p) {
+                if (p.hasOwnProperty(k)) {
+                    base[k] = p[k];
+                }
+            }
+        }
+    });
+    return base;
+}
+
 angular.module('charityApp')
   .factory('charitiesFactory', function ($http, $q) {
 
     var charitiesFactory = {}
 
+    var ref = new Firebase("https://tinder-charity.firebaseio.com/");
+
     charitiesFactory.getCharities = function() {
       var deferred = $q.defer();
-        // ref.child('users').child(authData.uid).on("value", function(snapshot) {
-        //     if (snapshot) {
-        //         deferred.resolve(snapshot.val());
-        //     } else {
-        //         deferred.resolve(null)
-        //     }
-        // });
+        ref.child('charities').on("value", function(charityShot) {
+            debugger
+            var userId = charityShot.val().id; // line 1 (results like 1,2,3,4,5,6)
+            ref.child('photos').child(userId).once("value", function(photoShot) {
+                console.log(id + ":" + mediaSnap.val().name);
+            })
+            // if (snapshot) {
+            //     // deferred.resolve(snapshot.val());
+            //     console.log(snapshot.val());
+            // } else {
+            //     console.log("The read failed: " + errorObject.code);
+            //     deferred.resolve(null)
+            // }
+        });
         return deferred.promise;
     }
 
